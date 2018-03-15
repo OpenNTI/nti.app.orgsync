cebaebafd68d4e5aca0a97eef930da5f8f10bc3c
Carlos Sanchez <carlos.sanchez@nextthought.com>
2018-03-15 17:32:36 -0500

    Initial checkin

---
+++ README.rst
@@ -0,0 +1,9 @@
+=================
+ nti.app.orgsync
+=================
+
+.. image:: https://travis-ci.org/NextThought/nti.app.orgsync.svg?branch=master
+    :target: https://travis-ci.org/NextThought/nti.app.orgsync
+
+.. image:: https://coveralls.io/repos/github/NextThought/nti.app.orgsync/badge.svg?branch=master
+    :target: https://coveralls.io/github/NextThought/nti.app.orgsync?branch=master
---
+++ src/nti/app/orgsync/tests/__init__.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from __future__ import division
+from __future__ import print_function
+from __future__ import absolute_import
+
+# pylint: disable=protected-access,too-many-public-methods,arguments-differ
+
+from zope import component
+
+from nti.app.testing.application_webtest import ApplicationTestLayer
+
+
+class OrgSyncApplicationTestLayer(ApplicationTestLayer):
+    pass
