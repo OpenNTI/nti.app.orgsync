13a5216129b032f0b53a2c36fc4276de6998c76b
Carlos Sanchez <carlos.sanchez@nextthought.com>
2018-04-06 14:07:37 -0500

    collapse tests into 1

--- src/nti/app/orgsync/tests/test_externalization.py
+++ src/nti/app/orgsync/tests/test_externalization.py
@@ -14,6 +14,8 @@ from hamcrest import has_entries
 import os
 import codecs
 
+import simplejson
+
 from zope import component
 
 from nti.app.orgsync.tests import OrgSyncApplicationTestLayer
@@ -26,11 +28,16 @@ from nti.externalization.externalization import to_external_object
 
 from nti.orgsync.accounts.utils import parse_account_source
 
+from nti.orgsync.entries.utils import membershiplog_object_hook
+from nti.orgsync.entries.utils import parse_membership_log_from_external
+
 from nti.orgsync.organizations.utils import parse_organization_source
 
 from nti.orgsync_rdbms.accounts.alchemy import load_accounts
 
 from nti.orgsync_rdbms.database.interfaces import IOrgSyncDatabase
+
+from nti.orgsync_rdbms.entries.alchemy import load_membership_logs
 
 from nti.orgsync_rdbms.organizations.alchemy import load_organizations
 
@@ -41,28 +48,40 @@ class TestExternalization(ApplicationLayerTest):
 
     @WithSharedApplicationMockDS(testapp=False, users=False)
     def test_org_external(self):
+        db = component.getUtility(IOrgSyncDatabase)
+
+        # org
         path = os.path.join(os.path.dirname(__file__), 'data',
                             "organization.json")
         with codecs.open(path, "r", "UTF-8") as fp:
             org = parse_organization_source(fp)
 
-        db = component.getUtility(IOrgSyncDatabase)
         orgs = load_organizations(db, (org,))
         assert_that(orgs, has_length(1))
         
         result = to_external_object(orgs[0])
         assert_that(result, has_entries('id', 152512))
         
-    @WithSharedApplicationMockDS(testapp=False, users=False)
-    def test_account_external(self):
+        # account
         path = os.path.join(os.path.dirname(__file__), 'data',
                             "account.json")
         with codecs.open(path, "r", "UTF-8") as fp:
             account = parse_account_source(fp)
 
-        db = component.getUtility(IOrgSyncDatabase)
         accounts = load_accounts(db, (account,))
-        assert_that(accounts, has_length(1))
-        
+        assert_that(accounts, has_length(1))        
         result = to_external_object(accounts[0])
         assert_that(result, has_entries('id', 7170547))
+        
+        # log
+        path = os.path.join(os.path.dirname(__file__), 'data',
+                            "membership_log.json")
+        with codecs.open(path, "r", "UTF-8") as fp:
+            data = membershiplog_object_hook(simplejson.load(fp))
+            entry = parse_membership_log_from_external(data)
+
+        entries = load_membership_logs(db, (entry,))
+        assert_that(entries, has_length(1))
+        
+        result = to_external_object(entries[0])
+        assert_that(result, has_entries('id', 59227531))
