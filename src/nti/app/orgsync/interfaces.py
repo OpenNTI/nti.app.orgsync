0018f0839b6269b5eb7a747b89a1db2f7a4fd167
Carlos Sanchez <carlos.sanchez@nextthought.com>
2018-04-26 17:23:35 -0500

    define event

--- src/nti/app/orgsync/interfaces.py
+++ src/nti/app/orgsync/interfaces.py
@@ -10,6 +10,11 @@ from __future__ import absolute_import
 
 # pylint: disable=inherit-non-class,no-value-for-parameter
 
+from zope import interface
+
+from zope.interface.interfaces import ObjectEvent
+from zope.interface.interfaces import IObjectEvent
+
 from zope.security.permission import Permission
 
 from nti.appserver.workspaces.interfaces import IWorkspace
@@ -17,6 +22,8 @@ from nti.appserver.workspaces.interfaces import IWorkspace
 from nti.dataserver.authorization import ROLE_PREFIX
 
 from nti.dataserver.authorization import StringRole
+
+from nti.schema.field import ValidDatetime
 
 #: The ID of a role for ourecomm
 RID_ORGSYNC_PREFIX = ROLE_PREFIX + 'nti.dataserver.orgsync'
@@ -42,3 +49,28 @@ class IOrgSyncWorkspace(IWorkspace):
     """
     A workspace containing data for orgsync.
     """
+
+
+class IOrgSyncSyncEvent(IObjectEvent):
+    """
+    An event that is sent, when the orgsync db is synchronized 
+    """
+
+    timestamp = ValidDatetime(title=u"The timestamp", 
+                              required=True)
+
+    start_date = ValidDatetime(title=u"Start date", 
+                               required=False)
+
+    end_date = ValidDatetime(title=u"Start date", 
+                             required=False)
+
+
+@interface.implementer(IOrgSyncSyncEvent)
+class OrgSyncSyncEvent(ObjectEvent):
+
+    def __init__(self, db, timestamp, start_date=None, end_date=None):
+        super(OrgSyncSyncEvent, self).__init__(db)
+        self.timestamp = timestamp
+        self.start_date = start_date
+        self.end_date = end_date
