15135255bdb4165f903ea46f2f0396aad1309b95
Carlos Sanchez <carlos.sanchez@nextthought.com>
2018-05-17 10:00:44 -0500

    remove admin view/code

--- src/nti/app/orgsync/views/sync_views.py
+++ src/nti/app/orgsync/views/sync_views.py
@@ -11,8 +11,6 @@ from __future__ import absolute_import
 from datetime import datetime
 from datetime import timedelta
 
-import isodate
-
 from requests.structures import CaseInsensitiveDict
 
 from pyramid.view import view_config
@@ -34,20 +32,15 @@ from nti.app.orgsync import SYNCHRONIZE
 
 from nti.app.orgsync.interfaces import ACT_SYNC_DB
 
-from nti.app.orgsync.synchronize import is_sync_lock_held
 from nti.app.orgsync.synchronize import create_orgsync_sync_job
 
 from nti.app.orgsync.views import OrgSyncPathAdapter
 
 from nti.orgsync.client import DEFAULT_MAX_WORKERS
 
-from nti.orgsync_rdbms.accounts.alchemy import Account
-
 from nti.orgsync_rdbms.database.interfaces import IOrgSyncDatabase
 
 from nti.orgsync_rdbms.entries.alchemy import MembershipLog
-
-from nti.orgsync_rdbms.organizations.alchemy import Organization
 
 from nti.orgsync_rdbms.utils import parse_date
 
@@ -98,57 +91,3 @@ class OrgSyncSyncView(AbstractAuthenticatedView,
         # pylint: disable=no-member
         creator = self.remoteUser.username
         return create_orgsync_sync_job(creator, start_date, end_date, workers)
-
-
-@view_config(name="sync")
-@view_config(name=SYNCHRONIZE)
-@view_config(context=OrgSyncPathAdapter)
-@view_defaults(route_name="objects.generic.traversal",
-               renderer="templates/synchronize.pt",
-               request_method="GET",
-               permission=ACT_SYNC_DB)
-class SynchronizationView(AbstractAuthenticatedView):
-
-    @Lazy
-    def database(self):
-        return component.getUtility(IOrgSyncDatabase)
-
-    @property
-    def accounts(self):
-        session = getattr(self.database, 'session', self.database)
-        return session.query(Account).count()
-
-    @property
-    def organizations(self):
-        session = getattr(self.database, 'session', self.database)
-        return session.query(Organization).count()
-
-    @property
-    def entries(self):
-        session = getattr(self.database, 'session', self.database)
-        return session.query(MembershipLog).count()
-
-    @property
-    def last_entry(self):
-        session = getattr(self.database, 'session', self.database)
-        # pylint: disable=no-member
-        entries = aliased(MembershipLog)
-        result = session.query(func.max(entries.created_at)).scalar()
-        if result is not None:
-            result = isodate.datetime_isoformat(result,
-                                                isodate.DATE_EXT_COMPLETE)
-        return result
-
-    def __call__(self):
-        # exclude final forward slash for join
-        context_url = self.request.resource_url(self.context)[:-1]
-        sync_url = "/".join((context_url, '@@synchronize'))
-        result = {
-            'sync_url': sync_url,
-            'entries': self.entries,
-            'accounts': self.accounts,
-            'last_entry': self.last_entry,
-            'lock_held': is_sync_lock_held(),
-            'organizations': self.organizations,
-        }
-        return result
